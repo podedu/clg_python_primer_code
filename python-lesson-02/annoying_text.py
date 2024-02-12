d15a58a9725a7e241838352cb90b3797cd7ea0ad
podedu <156981127+podedu@users.noreply.github.com>
2024-02-12 19:33:26 +1100

    Delete python-lesson-02/annoying_text.py

--- python-lesson-02/annoying_text.py
+++
@@ -1,6 +0,0 @@
-original_text = "My cat is a legend!"
-shouting_text = original_text.upper
-annoying_text = shouting_text * 10
-print (original_text)
-print (f"My annoying text is: {annoying_text}")
-print ("It has {len(annoying_text)} characters.")
