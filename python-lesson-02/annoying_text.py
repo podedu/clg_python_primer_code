59a048a7f8b9d80fcc97186f11b1228e7d6f2542
podedu <pauline.odonoghue@education.vic.gov.au>
2024-02-12 18:40:45 +1100

    Python lesson 2 assignment submission

---
+++ python-lesson-02/annoying_text.py
@@ -0,0 +1,6 @@
+original_text = "My cat is a legend!"
+shouting_text = original_text.upper
+annoying_text = shouting_text * 10
+print (original_text)
+print (f"My annoying text is: {annoying_text}")
+print ("It has {len(annoying_text)} characters.")
