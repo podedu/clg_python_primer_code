5e91c67f5eb29e8ee5c2fd43dc55097098322416
podedu <pauline.odonoghue@education.vic.gov.au>
2024-03-24 21:56:29 +1100

    Vowel counter updated to incorporate .lower() to resolve apostrophe causing incorrect result.

--- play/lesson_07_play_vowel_2.py
+++ play/lesson_07_play_vowel_2.py
@@ -2,7 +2,7 @@ def vowel_counting(name):
     vowel_count = 0    
     
     for char in name:
-        if char in ["a", "e", "i", "o", "u"]:
+        if char.lower() in ["a", "e", "i", "o", "u"]:
             vowel_count += 1
             
     print(f"My name is {name}. My name contains {vowel_count} vowels.")   
