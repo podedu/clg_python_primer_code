fa7016a640840dfb64ba4632e5b7ab3f3076df06
podedu <156981127+podedu@users.noreply.github.com>
2024-04-19 08:01:07 +1000

    Delete play directory

--- play/lesson.py
+++
@@ -1,6 +0,0 @@
-def check_palindrome(self):
-        text = self.entry_1.get()
-        if is_palindrome(text):
-            self.result_label.config(text="Well done! It's a palindrome!", fg="#e70d0d")
-        else:
-            self.result_label.config(text="Doh! It's not a palindrome.", fg="#800080")
